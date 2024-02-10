41ce941052b1ccfc7f23f136204719036312ff85
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-02-10 19:36:29 -0300

    fix: big avatar shadow switch

--- components/PageMain/bigAvatar.tsx
+++ components/PageMain/bigAvatar.tsx
@@ -1,5 +1,14 @@
 import { Avatar, AvatarGroup, AvatarIcon } from '@nextui-org/avatar';
+import YoFormal from '@public/image/YoFormal.png';
+import YoFormal2 from '@public/image/YoFormal2.png';
 
 export default function BigAvatar() {
-  return <Avatar />;
+  return (
+    <Avatar
+      src={YoFormal.src}
+      alt="AlfonsoM0 avatar"
+      className="w-60 h-60 text-large bg-transparent shadow-xl shadow-black dark:shadow-white"
+      isBordered
+    />
+  );
 }
--- components/PageMain/index.tsx
+++ components/PageMain/index.tsx
@@ -1,16 +1,9 @@
-import { Avatar, AvatarGroup, AvatarIcon } from '@nextui-org/avatar';
-import YoFormal from '@public/image/YoFormal.png';
-import YoFormal2 from '@public/image/YoFormal2.png';
+import BigAvatar from './bigAvatar';
 
 export default function PageMain() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <Avatar
-        src={YoFormal.src}
-        alt="AlfonsoM0 avatar"
-        className="w-60 h-60 text-large bg-transparent shadow-2xl shadow-black"
-        isBordered
-      />
+      <BigAvatar />
     </section>
   );
 }

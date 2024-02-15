cde006357702d9296bf19970ec1c94cc7a7bfc55
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-02-15 13:00:30 -0300

    fix: margins

--- components/PageMain/connect.tsx
+++ components/PageMain/connect.tsx
@@ -12,7 +12,7 @@ export default function Connect() {
   return (
     <div className="flex flex-col text-shadow shadow-white dark:shadow-black m-10">
       <h2 className={`text-center text-2xl`}>{txt.contactMe}</h2>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-4">
         {txt.contactMe_ul.map((item, i) => (
           <CustomPopover key={i} popoverTitle={item.name}>
             <Link href={item.href} className="m-3">
@@ -21,8 +21,8 @@ export default function Connect() {
           </CustomPopover>
         ))}
       </div>
-      <h3 className="text-lg">{txt.knowMore}</h3>
-      <Link href="/" className="text-center text-blue-800 dark:text-yellow-300">
+      <h3 className="text-lg mt-4">{txt.knowMore}</h3>
+      <Link href="/" className="text-center mt-4 text-blue-800 dark:text-yellow-300">
         {txt.alfonsoBot}
       </Link>
     </div>

c3fdd00552b6b4e0c60bdc02a049606e9aea0d69
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-02-20 22:27:41 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
+// https://ducanh-next-pwa.vercel.app/docs/next-pwa/configuring
+
 const withPWA = require('@ducanh2912/next-pwa').default({
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
   dest: 'public',
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    // document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
 });
 
-module.exports = withPWA({
-  // Your Next.js config
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
+
+module.exports = withPWA(nextConfig);

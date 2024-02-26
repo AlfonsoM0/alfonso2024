9597546af07ee601fe2ed0ad0247f4fb5d7a78d7
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-02-26 11:28:49 -0300

    fix: alfonsobot error handler

--- components/PageAlfonsobot/sendInput.tsx
+++ components/PageAlfonsobot/sendInput.tsx
@@ -3,29 +3,48 @@
 import runAlfonsobotChat from '@/server/actions/runAlfonsobotChat';
 import useUserStore from '@/store/userStore';
 import { Button, CircularProgress } from '@nextui-org/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function SendInput() {
-  const { appIsEnglish, alfonsobotChat, addChatResponse } = useUserStore((store) => store);
+  const { appIsEnglish, alfonsobotChat, addChatResponse, clearChatResponses } = useUserStore(
+    (store) => store
+  );
 
   const [inputText, setInputText] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
+  const [isBotError, setIsBotError] = useState(false);
+  const botErrorMsg = appIsEnglish
+    ? 'There was an error when processing your message. Check that it is not an inappropriate message and tries again.'
+    : 'Hubo un error al procesar tu mensaje. Comprueba que no sea un mensaje inapropiado e intenta nuevamente.';
+
   async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    setIsBotError(false);
     setIsLoading(true);
 
     addChatResponse({ role: 'user', parts: inputText });
 
-    const res = await runAlfonsobotChat(inputText, alfonsobotChat);
-    addChatResponse({ role: 'model', parts: res });
-
+    try {
+      const res = await runAlfonsobotChat(inputText, alfonsobotChat);
+      addChatResponse({ role: 'model', parts: res });
+    } catch (error) {
+      // console.error('Bot Error => ', error);
+      setIsBotError(true);
+      addChatResponse({ role: 'model', parts: botErrorMsg });
+    }
     setInputText('');
     setIsLoading(false);
   }
 
+  function reset() {
+    clearChatResponses();
+    setIsBotError(false);
+  }
+
   const buttonTxt = appIsEnglish ? 'Send' : 'Enviar';
   const placeholder = appIsEnglish ? 'Write your query...' : 'Escribe tu consulta...';
+
   return (
     <form onSubmit={onSubmit} className="flex items-center" aria-label={placeholder}>
       <input
@@ -54,6 +73,15 @@ export default function SendInput() {
           value={buttonTxt}
         />
       )}
+
+      {isBotError ? (
+        <button
+          onClick={reset}
+          className="bg-danger-600 dark:bg-danger-400 text-white font-medium py-2 px-4 rounded-full ml-1"
+        >
+          RESET
+        </button>
+      ) : null}
     </form>
   );
 }

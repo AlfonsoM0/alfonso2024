56b18225b45a1da343e142be98423c0ca8c01c0c
Alfonso, Montes de Oca <montesdeoca.alfonso@gmail.com>
2024-03-11 16:44:50 -0300

    fix: better navbar display

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -90,7 +90,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
+        <div className="mx-4 mt-10 flex flex-col items-center gap-10">
           {navLinks.map((item) => (
             <NavbarMenuItem key={item.url} aria-label={item.name}>
               <Link color="foreground" href={item.url} size="lg">
